70165a9ae9a263cafcbcc90470b67e6430592a3d
ELsawy <sawyahmed951@gmail.com>
2021-03-21 00:05:42 +0200

    adding permissions

--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render , redirect
 from django.http import HttpResponse ,HttpResponseRedirect
 from hello.models import Task, Movie, Category, Actor
 from .forms import editMovieForm
+from  django.contrib.auth.decorators import login_required, permission_required
 # Create your views here.
 
 tasks = {'1':'Go To Finalize your exam', '2':'finish this lab'}
@@ -28,14 +29,19 @@ def remove(request,Id):
     tasks.pop(f'{Id}')
     return redirect('tasks')
 
+@login_required
 def to_watch(request):
     return render(request , 'hello/toWatch.html',{'movies':Movie.objects.all()})
 
+@login_required
+@permission_required
 def removeMovie(request , ID):
     mov = Movie.objects.get(Id=ID)
     mov.delete()
     return redirect('towatch')
 
+@login_required
+@permission_required('hello.add_movie')
 def addMovie(request):
     if request.POST:
             movie = Movie(request.POST.get('movId'),request.POST.get('movTitle')
@@ -44,6 +50,8 @@ def addMovie(request):
             movie.save()
     return render(request,'hello/addMovie.html',{'categories': Category.objects.all()})
 
+@login_required
+@permission_required
 def editMovie(request,movId):
     movie = Movie.objects.get(Id=movId)
     return render(request,'hello/editMovie.html',{'movie':movie})
